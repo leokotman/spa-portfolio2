8cccd45c02a4c7f3967bd59b98b04a5c8061cd78
levkotman <leoktmn@yandex.ru>
2021-09-10 16:05:23 +0200

    styled contacts v-card v-list

--- src/components/contacts/ContactsPage.vue
+++ src/components/contacts/ContactsPage.vue
@@ -1,22 +1,53 @@
 <template>
   <main>
     <h1>Contacts page</h1>
-    <ul>
-      <li v-for="contact in contacts" :key="contact.index">
-        <a :href="'tel:+' + contact">{{ contact }}</a>
-      </li>
-    </ul>
-    <ul>
-      <li v-for="link in webLinks" :key="link.name">
-        <a :href="link.link">{{ link.name }}</a>
-      </li>
-    </ul>
+    <v-card tile max-width="300">
+      <v-list rounded>
+        <v-list-item-group
+          ><v-list-item v-for="contact in contacts" :key="contact.index">
+            <v-list-item-icon>
+              <v-icon v-text="icons.contacts"></v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>
+                <a :href="'tel:' + contact">
+                  {{ contact }}
+                </a>
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-card>
+    <v-card tile max-width="300">
+      <v-list rounded>
+        <v-list-item-group
+          ><v-list-item v-for="link in webLinks" :key="link.name">
+            <v-list-item-icon>
+              <v-icon v-text="icons.links"></v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>
+                <a :href="link.link">
+                  {{ link.name }}
+                </a>
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-card>
   </main>
 </template>
 
 <script>
 export default {
   inject: ["address", "contacts", "webLinks"],
+  data() {
+    return {
+      icons: { contacts: "mdi-phone", links: "mdi-web" },
+    };
+  },
 };
 </script>
 
@@ -29,5 +60,21 @@ main {
 h1 {
   width: 100%;
   text-align: center;
+}
+
+.v-list-item {
+  min-height: 60px;
+  background-color: rgb(248, 246, 246);
+}
+.v-list-item--active {
+  background-color: lightblue;
+}
+.v-list-item--active a {
+  color: white;
+}
+
+a {
+  text-decoration: none;
+  color:darkblue;
 }
 </style>
