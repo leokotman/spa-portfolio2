3d406ccd3baca663d1feb2f357a2cfe8ceb97340
levkotman <leoktmn@yandex.ru>
2021-09-12 03:18:27 +0200

    added games to projects

--- src/components/projects/GamesProjects.vue
+++ src/components/projects/GamesProjects.vue
@@ -1,9 +1,38 @@
 <template>
   <section>
-    <h2>Games</h2>
     <ul>
-      <li>game 1</li>
-      <li>game 2</li>
+      <li>
+        <v-card>
+          <a href="https://leokotman.github.io/monster-game/" target="_blank">
+            <img
+              src="../../assets/img/game-monster.png"
+              alt="monster-game"
+              height="400"/>
+          </a>
+        </v-card>
+      </li>
+      <li>
+        <v-card>
+          <a href="https://leokotman.github.io/doodle-jump/" target="_blank">
+            <img
+              src="../../assets/img/game-jump.png"
+              alt="jump-game"
+              height="400"/>
+          </a>
+        </v-card>
+      </li>
     </ul>
   </section>
 </template>
+
+<style scoped>
+ul {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  gap: 2rem;
+}
+ul li {
+  list-style-type: none;
+}
+</style>
